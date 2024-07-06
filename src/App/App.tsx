ce731a7b16df7fcc1f231b89b7ba53e1c4645df2
dmitry_surdin <dimasurdin@yandex.ru>
2024-07-07 03:14:38 +0400

    add react toast

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,10 @@
 import { FC } from 'react';
+import { ToastContainer } from 'react-toastify';
 import { AppRouter } from './components/AppRouter/AppRouter';
 
-export const App: FC = () => <AppRouter />;
+export const App: FC = () => (
+  <>
+    <AppRouter />
+    <ToastContainer position="bottom-left" closeOnClick pauseOnHover />
+  </>
+);
