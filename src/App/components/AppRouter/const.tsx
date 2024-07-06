0a2963d591abf1571a16d268fb603077bd83bfe5
dmitry_surdin <dimasurdin@yandex.ru>
2024-07-06 21:49:35 +0400

    add baseUrl to tsconfig.json, fix imports

--- src/App/components/AppRouter/const.tsx
+++ src/App/components/AppRouter/const.tsx
@@ -1,9 +1,9 @@
 import { RouteObject } from 'react-router';
-import { CommonLayout } from '../../../components/layouts/CommonLayout';
-import { Dashboard } from '../../../pages/Dashboard';
-import { Character } from '../../../pages/Character';
-import { AppRoutes } from '../../../const/AppRoutes';
-import { NotFound } from '../../../pages/NoFound';
+import { CommonLayout } from 'components/layouts/CommonLayout';
+import { Dashboard } from 'pages/Dashboard';
+import { Character } from 'pages/Character';
+import { AppRoutes } from 'const/AppRoutes';
+import { NotFound } from 'pages/NoFound';
 
 export const routes: Array<RouteObject> = [
   {
